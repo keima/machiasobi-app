cfc96ba570d7fd2058c8d7097103525e844971cc
Kouta Imanaka <k.imanaka@moggy.in>
2014-05-07 18:40:45 +0900

    targetBlankのDirectiveをリファクタリング

--- app/scripts/directive.js
+++ app/scripts/directive.js
@@ -2,7 +2,6 @@ angular.module('myApp.directive', [])
   // @see http://stackoverflow.com/questions/20099784/open-links-in-new-window-using-angularjs
   .directive('targetBlank', function () {
     return {
-      priority: -9999,
       restrict: 'A',
       link: function (scope, element, attr) {
         if (element.prop("tagName") === 'A') {
@@ -10,7 +9,7 @@ angular.module('myApp.directive', [])
         } else {
           var elems = element.find('A');
           element.bind('DOMSubtreeModified', function () {
-            elems = $(this).find('A');
+            elems = angular.element(this).find('A');
             elems.attr("target", "_blank");
           });
         }
