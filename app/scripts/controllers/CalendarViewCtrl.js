a6267f78a226c2e1d9e62ffc14dcbb2327a37914
Kouta Imanaka <keima@p-side.net>
2014-07-08 00:57:55 +0900

    凡例クリックすると、そのカレンダーだけを表示する機能を追加

--- app/scripts/controllers/CalendarViewCtrl.js
+++ app/scripts/controllers/CalendarViewCtrl.js
@@ -1,20 +1,20 @@
 angular.module('myApp.controller.calendarViewCtrl', [])
   .controller('CalendarViewCtrl',
-  function($scope,$rootScope, $timeout, Calendar, EventStore) {
+  function ($scope, $rootScope, $timeout, Calendar, EventStore) {
 
     /**
      * Return selected id
      * @param current
      * @param periods
      */
-    var defaultSelected = function(current, periods) {
+    var defaultSelected = function (current, periods) {
       console.log(current.startOf('day').toISOString());
       console.log(periods[0].date.startOf('day').toISOString());
-      if (current.isSame(periods[0].date,'day')) {
+      if (current.isSame(periods[0].date, 'day')) {
         return 0;
-      } else if (current.isSame(periods[1].date,'day')) {
+      } else if (current.isSame(periods[1].date, 'day')) {
         return 1;
-      } else if (current.isSame(periods[2].date,'day')) {
+      } else if (current.isSame(periods[2].date, 'day')) {
         return 2;
       } else {
         return 0;
@@ -32,33 +32,41 @@ angular.module('myApp.controller.calendarViewCtrl', [])
     }
     $rootScope.lastVersion = $rootScope.semver;
 
-    $scope.selected = defaultSelected(moment(), $rootScope.periods);
+    $scope.selectedDateId = defaultSelected(moment(), $rootScope.periods);
     $scope.eventSources = Calendar.buildSources($scope.calendars);
     var originEventSources = _.cloneDeep($scope.eventSources);
 
     // イベントのクリックリスナ
-    $scope.calendarConfig.eventClick = function(event, jsEvent, view){
+    $scope.calendarConfig.eventClick = function (event, jsEvent, view) {
       EventStore.save(event);
       $scope.ons.screen.presentPage('event.html');
     };
 
-    // 日付ボタンバーのクリックイベント TODO:名前。。。
-    $scope.setSelected = function(id) {
-      $scope.selected = id;
+    // 日付ボタンバーのクリックイベント
+    $scope.setSelectedDateId = function (id) {
+      $scope.selectedDateId = id;
     };
 
-    $scope.$watch('selected', function(value) {
+    // 凡例のクリックイベント
+    $scope.showOnlyThisEvent = function (calendarIndex) {
+      $scope.calendars.forEach(function (element, index) {
+        element.selected = (calendarIndex == index);
+      });
+      $rootScope.$broadcast('eventSourceIsChanged');
+    };
+
+    $scope.$watch('selectedDateId', function (value) {
       $scope.calendarConfig.year = $scope.periods[value].date.year();
       $scope.calendarConfig.month = $scope.periods[value].date.months();
       $scope.calendarConfig.date = $scope.periods[value].date.date();
     });
 
-    $scope.$on('eventSourceIsChanged', function() {
+    $scope.$on('eventSourceIsChanged', function () {
       // 全消し
       $scope.eventSources.splice(0, $scope.eventSources.length);
 
       // selected:trueのものだけ詰め直し
-      $scope.calendars.forEach(function(element, index) {
+      $scope.calendars.forEach(function (element, index) {
         if (element.selected) {
           $scope.eventSources.push(originEventSources[index]);
         }
@@ -73,7 +81,7 @@ angular.module('myApp.controller.calendarViewCtrl', [])
 
     // SUPER-DIRTY-HACK!!!!!!!!
     // カレンダーの高さを 画面高 - もろもろのパーツ しつつレンダリングする
-    var renderCalendar = function(){
+    var renderCalendar = function () {
       var _calendar = $('#calendar');
       $scope.calendarConfig.height = _calendar.height() - $('#legends').height() - $('#dateSelector').height();
       _calendar.fullCalendar('render');
