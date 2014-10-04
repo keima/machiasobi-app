ed3159b31a12861c3e1c2ef0f88fa1ebac1038a0
Kouta Imanaka <keima@p-side.net>
2014-10-04 13:01:14 +0900

    次回持ち越し案件

--- app/scripts/controllers/RootCtrl.js
+++ app/scripts/controllers/RootCtrl.js
@@ -3,8 +3,8 @@ angular.module('myApp.controller.rootCtrl', [])
     $scope.appList = [
       {name: 'カレンダー', icon: 'ion-calendar', state: 'calendar',
         description: "マチ★アソビのイベントをカレンダー形式で表示します"},
-      {name: '待ち時間', icon: 'ion-clock', state: 'traffic',
-        description: "ロープウェイ・シャトルバスの乗り場の待ち時間情報を表示します"},
+//      {name: '待ち時間', icon: 'ion-clock', state: 'traffic',
+//        description: "ロープウェイ・シャトルバスの乗り場の待ち時間情報を表示します"},
       {name: 'Twitter', icon: 'ion-social-twitter', state: 'twitter',
         description: "マチ★アソビに関するTwitter情報を見ることが出来ます"},
       {name: 'マップ', icon: 'ion-map', state: 'map',
