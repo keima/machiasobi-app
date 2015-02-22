19694706068c4063befe945ee18fb14424060aff
Kouta Imanaka <keima@p-side.net>
2015-02-22 19:33:55 +0900

    地図情報のマスタデータをmachitoolsから参照するようにした

--- app/scripts/services/machitools.js
+++ app/scripts/services/machitools.js
@@ -1,6 +1,10 @@
+"use strict";
+
 angular.module('myApp.service.machitools', [])
   .factory('MachiRest', function (Restangular) {
     return Restangular.withConfig(function (config) {
       config.setBaseUrl('http://machiasobi-tools.appspot.com/api/v1');
+        //config.setBaseUrl('http://localhost:8080/api/v1');
     });
-  });
+  })
+;
