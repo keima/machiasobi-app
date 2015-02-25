adcd88c946b1b534eef7bda6435695e3187c6945
Kouta Imanaka <keima@p-side.net>
2015-02-26 05:14:12 +0900

    target-blank ディレクティブのAnalytics計測

--- app/scripts/directive.js
+++ app/scripts/directive.js
@@ -1,19 +1,32 @@
-angular.module('myApp.directive', [])
+angular.module('myApp.directive', ['angulartics'])
 /**
- * aタグのリンクを新しいタブで開くようにします。
+ * aタグのリンクを新しいタブで開くようにします。ついでにangularticsで計測も行います。
  * @see http://stackoverflow.com/questions/20099784/open-links-in-new-window-using-angularjs
  */
-  .directive('targetBlank', function () {
+  .directive('targetBlank', function ($analytics) {
+
+    function bindTo(element) {
+      element.attr("target", "_blank");
+
+      element.bind("click", function () {
+        $analytics.eventTrack('Click', {
+          category: 'A Tag Link',
+          label: element.attr("href"),
+          noninteraction: true
+        });
+      });
+    }
+
     return {
       restrict: 'A',
       link: function (scope, element, attr) {
         if (element.prop("tagName") === 'A') {
-          element.attr("target", "_blank");
+          bindTo(element);
         } else {
           var elems = element.find('A');
           element.bind('DOMSubtreeModified', function () {
             elems = angular.element(this).find('A');
-            elems.attr("target", "_blank");
+            bindTo(elems);
           });
         }
       }
